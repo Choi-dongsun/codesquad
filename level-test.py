638c44295143d57e3ad7c3143ed027e71b6b4aaa
Choi-dongsun <rovks91@gmail.com>
2018-08-29 23:48:48 +0900

    fiexd, after visiting codesqaud.

--- level-test.py
+++ level-test.py
@@ -8,12 +8,11 @@ def game_start():
     while True:
         user = user_choice()
         result = check(user, com)
-        if result[0] == 3:
-            print_result(result)
-            game_start()
-        else:
-            print_result(result)
-        
+
+        strike = result_strike(result)
+        ball = result_ball(result, strike)
+        result_total(strike, ball)
+
 
 # 컴퓨터의 세수 뽑기
 def com_choice():
@@ -39,15 +38,19 @@ def user_choice():
 
     while True:
         user = input('숫자를 입력해주세요. ex)123 : ')
-        if len(user) != 3:
-            print("올바른 수를 입력해주세요")
+        if user.isdigit() == False:
+            print("문자나 기호가 아닌 숫자를 입력해주세요")
+            continue
+        elif len(user) != 3:
+            print("세자리 수를 입력해주세요")
             continue
         elif user[0] == user[1] or user[0] == user[2] or user[1] == user[2]:
             print("서로 다른 수를 입력해주세요")
             continue
         else:
             break
-
+            
+    user = [int(i) for i in user]        
     return user
 
 
@@ -59,31 +62,48 @@ def check(user, com):
     i = 0
 
     while i < 3:
-        if int(user[i]) == com[i]:
+        if user[i] == com[i]:
             strike += 1
-        elif int(user[i]) in com:
+        elif user[i] in com:
             ball += 1
         i += 1
     
     return strike, ball
 
 
-# 결과 발표
-def print_result(result):
+# 스트라이크 단독 결과 처리
+def result_strike(result):
 
     strike = result[0]
     ball = result[1]
 
-    if strike == 0 and ball == 0:
-        print("포볼")
-    elif strike == 3:
-        print("%d 스트라이크" % strike)
+    if strike == 3:
+        print("3스트라이크! ")
         print("3개의 숫자를 모두 맞히셨습니다! 축하합니다.")
+        game_start()
     elif strike > 0 and ball == 0:
         print("%d 스트라이크" % strike)
-    elif ball > 0 and strike == 0:
+    
+    return strike
+
+
+# 볼 단독 결과 처리
+def result_ball(result, strike):
+
+    ball = result[1]
+
+    if ball > 0 and strike == 0:
         print("%d볼" % ball)
-    else:
+
+    return ball
+
+
+# 스트라이크, 볼 혼합 결과 처리
+def result_total(strike, ball):
+
+    if strike == 0 and ball == 0:
+        print("포볼")
+    elif strike > 0 and ball > 0:
         print("%d 스트라이크 %d볼" % (strike, ball))
 
 
